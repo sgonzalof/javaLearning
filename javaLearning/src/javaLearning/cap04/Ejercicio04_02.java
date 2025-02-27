b48ca869069ae928bedeb63c9a060dcab66e3a94
sgonzalof <sergiogonzafeli@gmail>
2025-02-27 10:19:40 +0100

    comentarios en 04_02

--- javaLearning/src/javaLearning/cap04/Ejercicio04_02.java
+++ javaLearning/src/javaLearning/cap04/Ejercicio04_02.java
@@ -1,11 +1,15 @@
+/* Pintar los numero de 0 a 99 en una matriz de 10 por 10 utilizando bucles for */
+
 public class Ejercicio04_02 {
 
-
-
+    // Método que llena una matriz de 10x10 con números consecutivos y los imprime
     public static void tablaConsecutivos(int numeroInicial, int longitud) {
+        // Declaración de una matriz de 10x10
         int[][] num = new int[10][10];
+        // Variable para llevar el conteo de los números consecutivos
         int x = numeroInicial;
 
+        // Llenar la matriz con números consecutivos
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 num[i][j] = x;
@@ -13,19 +17,23 @@ public class Ejercicio04_02 {
             }
         }
 
+        // Imprimir la matriz
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (i==0){
-                    System.out.print(" "+num[i][j] + " ");
-                }
-                else{
-                System.out.print(num[i][j] + " ");
+                // Si es la primera fila, imprimir con un espacio adicional al inicio
+                if (i == 0) {
+                    System.out.print(" " + num[i][j] + " ");
+                } else {
+                    // Para las demás filas, imprimir normalmente
+                    System.out.print(num[i][j] + " ");
                 }
             }
+            // Imprimir una nueva línea después de cada fila
             System.out.println();
         }
     }
 
+    // Método principal que llama a tablaConsecutivos con los parámetros iniciales
     public static void main(String[] args) {
         tablaConsecutivos(0, 99);
     }
